3d8c3e860912ce6adcbb8ccac3ff5af2a2e7ad0f
davidvk89 <david@dkvdev.com>
2023-04-30 11:50:33 +0200

    activate event listeners for register input

--- view/widgets/register/app.js
+++ view/widgets/register/app.js
@@ -5,30 +5,37 @@ const widgetRegister_password = document.getElementById('registerAccountPassword
 const widgetRegister_submit = document.getElementById('registerAccountSubmit');
 const widgetRegister_responseContainer = document.getElementById('widgetRegister_responseContainer');
 
-// widgetRegister_email.addEventListener('keypress', function(event){
-//     if(event.key === 'Enter'){
-//         event.preventDefault();
-//         widget_register();
-//     }
-// });
-// widgetRegister_username.addEventListener('keypress', function(event){
-//     if(event.key === 'Enter'){
-//        event.preventDefault();
-//        widget_register();
-//     }
-// });
-// widgetRegister_password.addEventListener('keypress', function(event){
-//     if(event.key === 'Enter'){
-//         event.preventDefault();
-//         widget_register();
-//     }
-// });
+widgetRegister_email.addEventListener('keypress', function(event){
+    if(event.key === 'Enter'){
+        event.preventDefault();
+        widget_register();
+    }
+});
+
+widgetRegister_username.addEventListener('keypress', function(event){
+    if(event.key === 'Enter'){
+       event.preventDefault();
+       widget_register();
+    }
+});
+
+widgetRegister_password.addEventListener('keypress', function(event){
+    if(event.key === 'Enter'){
+        event.preventDefault();
+        widget_register();
+    }
+});
+
 
 function widget_register(){
+   // @ts-ignore
    $.post('api/auth/register.php', {
     register: true,
+    // @ts-ignore
     email: widgetRegister_email.value,
+    // @ts-ignore
     account: widgetRegister_username.value,
+    // @ts-ignore
     password: widgetRegister_password.value
    },
    function(response){
